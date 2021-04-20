699a6666a92bc671639d7e0672ab70a980d80064
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-04-20 21:49:19 +0900

    インデント修正

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
   describe "GET #show" do
-    let(:product) {create(:product)}
+    let(:product) { create(:product) }
+
     before do
       get potepan_product_path(product.id)
     end
+
     it "returns http success" do
       expect(response).to have_http_status(:success)
     end
@@ -15,5 +17,4 @@ RSpec.describe "Potepan::Products", type: :request do
       expect(response.body).to include product.description
     end
   end
-
 end
