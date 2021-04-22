f6f8d3f49ded7a9139059d9768f230b6931d010d
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-04-22 17:29:31 +0900

    rspec表示のテストを削除

--- spec/requests/potepan/products_spec.rb
+++ spec/requests/potepan/products_spec.rb
@@ -11,10 +11,5 @@ RSpec.describe "Potepan::Products", type: :request do
     it "returns http success" do
       expect(response).to have_http_status(:success)
     end
-    it "show templete products display" do
-      expect(response.body).to include product.name
-      expect(response.body).to include product.display_price.to_s
-      expect(response.body).to include product.description
-    end
   end
 end
