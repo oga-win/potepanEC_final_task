4b6712ddbaeccf2adf50121b0728cb22986aba89
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-04-23 13:21:13 +0900

    Base_title定数で定義,nilを考慮しempty?→blank?に変更,if内文字列変数展開を使用

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
+Base_title = "BIGBAG Store"
+
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "BIGBAG Store"
-    if page_title.empty?
-      base_title
+    if page_title.blank?
+      "#{Base_title}"
     else
-      page_title + " - " + base_title
+      "#{page_title} - #{Base_title}"
     end
   end
 end
