c86d19681a3f760ba1cb798983614578a8a7e673
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-04-25 17:48:30 +0900

    BASE_TITLE大文字&配置場所変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,10 @@
-Base_title = "BIGBAG Store".freeze
-
 module ApplicationHelper
+  BASE_TITLE = "BIGBAG Store".freeze
   def full_title(page_title)
     if page_title.blank?
-      "#{Base_title}"
+      BASE_TITLE
     else
-      "#{page_title} - #{Base_title}"
+      "#{page_title} - #{BASE_TITLE}"
     end
   end
 end
