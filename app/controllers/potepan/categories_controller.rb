a69b54708cd3f4ee9b9223357cd5df28cc5f19dc
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-03 12:53:00 +0900

    side bar実装

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,6 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.products
+    @products = @taxon.all_products
+    @taxonomies = Spree::Taxon.roots
   end
 end
