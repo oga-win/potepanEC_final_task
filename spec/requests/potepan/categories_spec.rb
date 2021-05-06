67d824943a06891119575cfa4127321e3ab31c17
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-06 15:33:45 +0900

    describe categories spec

--- spec/requests/potepan/categories_spec.rb
+++ spec/requests/potepan/categories_spec.rb
@@ -1,11 +1,27 @@
-#require 'rails_helper'
+RSpec.describe "Potepan::Categories", type: :request do
+  describe "GET /show" do
+    let!(:product) { create(:product, taxons:[taxon] ) }
+    let(:taxonomy) { create(:taxonomy) }
+    let(:taxon) { create(:taxon, parent_id: taxonomy.root.id, taxonomy: taxonomy) }
 
-#RSpec.describe "Potepan::Categories", type: :request do
-#  describe "GET /show" do
-#    it "returns http success" do
-#      get "/potepan/categories/show"
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
-
-#end
+    before do
+        get potepan_category_path(taxon.id)
+      end
+  
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+      it 'taxon name can be displayed' do
+        expect(response.body).to include(taxon.name)
+      end
+      it 'taxonomy name can be displayed' do
+        expect(response.body).to include(taxonomy.name)
+      end
+      it 'product name can be displayed' do
+        expect(response.body).to include(product.name)
+      end
+      it 'product price can be displayed' do
+        expect(response.body).to include(product.display_price.to_s)
+      end
+  end
+end
