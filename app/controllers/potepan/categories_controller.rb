0a9726ab4016d4accd16cbbb6362a504aed50ab8
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-09 21:06:55 +0900

    bullet導入,n+1問題修正

--- app/controllers/potepan/categories_controller.rb
+++ app/controllers/potepan/categories_controller.rb
@@ -1,7 +1,7 @@
 class Potepan::CategoriesController < ApplicationController
   def show
     @taxon = Spree::Taxon.find(params[:id])
-    @products = @taxon.all_products
+    @products = Spree::Product.includes(master: [:images, :default_price]).in_taxon(@taxon)
     @taxonomies = Spree::Taxon.roots
   end
 end
