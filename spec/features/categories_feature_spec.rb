a3a4511d42a79323a22dc7e8f90d332e50c83d6a
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-11 01:15:13 +0900

    describe feature_spec&n+1問題修正

--- spec/features/categories_feature_spec.rb
+++ spec/features/categories_feature_spec.rb
@@ -1,5 +1,91 @@
 require 'rails_helper'
 
 RSpec.feature "CategoriesFeatures", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:taxonomy) { create(:taxonomy, name: "Category") }
+  let!(:taxon) { create(:taxon, parent: taxonomy.root) }
+  let!(:bag) { create(:taxon, name: "bag", taxonomy: taxonomy, parent: taxonomy.root) }
+  let!(:shirts) { create(:taxon, name: "Shirts", taxonomy: taxonomy, parent: taxon) }
+  let!(:product) do
+    create(:product, name: "Ruby on Rails T-Shirt", price: "23.45", taxons: [shirts])
+  end
+
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_link product.name, href: potepan_product_path(product.id)
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(current_path).to eq potepan_category_path(taxonomy.root.id)
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_title "#{taxonomy.root.name} - BIGBAG Store"
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".page-title h2", text: "Category"
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".col-xs-6 .breadcrumb", text: "Home Shop Category"
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".panel-body", text: bag.name
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".panel-body", text: shirts.name
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".productCaption h5", text: product.name
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_selector ".productCaption h3", text: product.display_price
+  end
+  scenario "View category show page" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_link "Home", href: potepan_path
+  end
+
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(current_path).to eq potepan_category_path(bag.id)
+  end
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(page).to have_title "#{bag.name} - BIGBAG Store"
+  end
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(page).not_to have_selector ".page-title h2", text: "clothing"
+  end
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(page).to have_selector ".page-title h2", text: "bag"
+  end
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(page).not_to have_selector ".productCaption h5", text: product.name
+  end
+  scenario "Products for each category are displayed" do
+    visit potepan_category_path(bag.id)
+    expect(page).not_to have_selector ".productCaption h3", text: product.display_price
+  end
+
+  scenario "should be able to visit other category" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_link bag.name, href: potepan_category_path(bag.id)
+  end
+  scenario "should be able to visit other category" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_link shirts.name, href: potepan_category_path(shirts.id)
+  end
+
+  scenario "should be able to visit products" do
+    visit potepan_category_path(taxonomy.root.id)
+    expect(page).to have_link product.name, href: potepan_product_path(product.id)
+  end
 end
--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature "ProductsFeatures", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let!(:taxonomy) { create(:taxonomy, name: "Category") }
+  let!(:taxon) do
+    create(:taxon, name: "Taxon", taxonomy: taxonomy, parent: taxonomy.root)
+  end
+
+  let!(:product) { create(:product, taxons: [taxon]) }
+
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_current_path potepan_product_path(product.id)
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_title "#{product.name} - BIGBAG Store"
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".page-title h2", text: product.name
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".col-xs-6 h2", text: product.name
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".col-xs-6 h2", text: product.name
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".media-body h2", text: product.name
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".media-body h3", text: product.display_price
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_selector ".media-body p", text: product.description
+  end
+  scenario "View show page" do
+    visit potepan_product_path(product.id)
+    expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
+  end
 end
