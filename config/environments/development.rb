46db133054df8aef02b1bb249bec596ad2ca38e3
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-11 16:32:31 +0900

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,8 +68,8 @@ Rails.application.configure do
   config.after_initialize do
     Bullet.enable = true
     Bullet.alert = true
-    Bullet.bullet_logger = true
-    Bullet.console = true
-    Bullet.rails_logger = true
+    #Bullet.bullet_logger = true
+    #Bullet.console = true
+    #Bullet.rails_logger = true
   end
 end
