96375c4898e68c36452d5fe2d0e6fd034fbae97b
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-14 15:39:47 +0900

    共通部をbeforeで統一,説明文修正

--- spec/features/categories_feature_spec.rb
+++ spec/features/categories_feature_spec.rb
@@ -9,93 +9,71 @@ RSpec.feature "CategoriesFeatures", type: :feature do
     create(:product, name: "Ruby on Rails T-Shirt", price: "23.45", taxons: [shirts])
   end
 
-  scenario "View category link" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_link product.name, href: potepan_product_path(product.id)
+  describe "About taxonomy" do
+    before { visit potepan_category_path(taxonomy.root.id) }
+
+    scenario "View category page" do
+      expect(current_path).to eq potepan_category_path(taxonomy.root.id)
+    end
+
+    scenario "Show page-title on the category page" do
+      expect(page).to have_title "#{taxonomy.root.name} - BIGBAG Store"
+    end
+
+    scenario "Show sidebar-title on the category page" do
+      expect(page).to have_selector ".page-title h2", text: "Category"
+    end
+
+    scenario "Show breadcrumbs on the category page" do
+      expect(page).to have_selector ".col-xs-6 .breadcrumb", text: "Home Shop Category"
+    end
+
+    scenario "List products that belong to the same taxon" do
+      expect(page).to have_selector ".panel-body", text: bag.name
+      expect(page).to have_selector ".panel-body", text: shirts.name
+    end
+
+    scenario "Show product-name on the category page" do
+      expect(page).to have_selector ".productCaption h5", text: product.name
+    end
+
+    scenario "Show product-price on the category page" do
+      expect(page).to have_selector ".productCaption h3", text: product.display_price
+    end
+
+    scenario "The link to the top page is correct" do
+      expect(page).to have_link "Home", href: potepan_path
+    end
+
+    scenario "The link to list products that belong to the same taxon is correct" do
+      expect(page).to have_link bag.name, href: potepan_category_path(bag.id)
+      expect(page).to have_link shirts.name, href: potepan_category_path(shirts.id)
+    end
+
+    scenario "The link to the product page is correct" do
+      expect(page).to have_link product.name, href: potepan_product_path(product.id)
+    end
   end
 
-  scenario "View category page" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(current_path).to eq potepan_category_path(taxonomy.root.id)
-  end
+  describe "About bag" do
+    before { visit potepan_category_path(bag.id) }
 
-  scenario "View category page title" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_title "#{taxonomy.root.name} - BIGBAG Store"
-  end
+    scenario "Bag page display" do
+      expect(current_path).to eq potepan_category_path(bag.id)
+    end
 
-  scenario "View category page sidebar-title" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".page-title h2", text: "Category"
-  end
+    scenario "Show page-title on the bag page" do
+      expect(page).to have_title "#{bag.name} - BIGBAG Store"
+    end
 
-  scenario "View category page breadcrumb" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".col-xs-6 .breadcrumb", text: "Home Shop Category"
-  end
+    scenario "Show right section text on the bag page" do
+      expect(page).not_to have_selector ".page-title h2", text: "clothing"
+      expect(page).to have_selector ".page-title h2", text: "bag"
+    end
 
-  scenario "View category page bag-name" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".panel-body", text: bag.name
-  end
-
-  scenario "View category page shirts-name" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".panel-body", text: shirts.name
-  end
-
-  scenario "View category page product-name" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".productCaption h5", text: product.name
-  end
-
-  scenario "View category page product-price" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_selector ".productCaption h3", text: product.display_price
-  end
-
-  scenario "View category page Home link path" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_link "Home", href: potepan_path
-  end
-
-  scenario "Bag page display" do
-    visit potepan_category_path(bag.id)
-    expect(current_path).to eq potepan_category_path(bag.id)
-  end
-
-  scenario "Bag page title display" do
-    visit potepan_category_path(bag.id)
-    expect(page).to have_title "#{bag.name} - BIGBAG Store"
-  end
-
-  scenario "Bag page category text display" do
-    visit potepan_category_path(bag.id)
-    expect(page).not_to have_selector ".page-title h2", text: "clothing"
-  end
-
-  scenario "Bag page bag text display" do
-    visit potepan_category_path(bag.id)
-    expect(page).to have_selector ".page-title h2", text: "bag"
-  end
-
-  scenario "Bag page product name display" do
-    visit potepan_category_path(bag.id)
-    expect(page).not_to have_selector ".productCaption h5", text: product.name
-  end
-
-  scenario "Bag page product price display" do
-    visit potepan_category_path(bag.id)
-    expect(page).not_to have_selector ".productCaption h3", text: product.display_price
-  end
-
-  scenario "View bag page link" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_link bag.name, href: potepan_category_path(bag.id)
-  end
-
-  scenario "View shirts page link" do
-    visit potepan_category_path(taxonomy.root.id)
-    expect(page).to have_link shirts.name, href: potepan_category_path(shirts.id)
+    scenario "Show production caption on the bag page" do
+      expect(page).not_to have_selector ".productCaption h5", text: product.name
+      expect(page).not_to have_selector ".productCaption h3", text: product.display_price
+    end
   end
 end
