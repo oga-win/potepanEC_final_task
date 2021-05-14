09eac738372eac78a0b6df2e86cff078727d5383
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-14 16:40:14 +0900

    product_feature_spec 共通部をbeforeで統一,説明文修正

--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -7,43 +7,33 @@ RSpec.feature "ProductsFeatures", type: :feature do
   end
   let!(:product) { create(:product, taxons: [taxon]) }
 
-  scenario "View show page" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_current_path potepan_product_path(product.id)
-  end
+  describe "About taxonomy" do
+    before { visit potepan_product_path(product.id) }
 
-  scenario "View title" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_title "#{product.name} - BIGBAG Store"
-  end
-
-  scenario "View show page-title product-name" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_selector ".page-title h2", text: product.name
-  end
-
-  scenario "View right-section product name for product page" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_selector ".col-xs-6 h2", text: product.name
-  end
-
-  scenario "View media-body product name for product page" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_selector ".media-body h2", text: product.name
-  end
-
-  scenario "View product-display_price" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_selector ".media-body h3", text: product.display_price
-  end
-
-  scenario "View product-description" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_selector ".media-body p", text: product.description
-  end
-
-  scenario "have_link back index-page" do
-    visit potepan_product_path(product.id)
-    expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
+    scenario "View show page" do
+      expect(page).to have_current_path potepan_product_path(product.id)
+    end
+  
+    scenario "Show page title for product page" do
+      expect(page).to have_title "#{product.name} - BIGBAG Store"
+    end
+  
+    scenario "The page title for product page" do
+      expect(page).to have_selector ".page-title h2", text: product.name
+    end
+  
+    scenario "Show right section product name for product page" do
+      expect(page).to have_selector ".col-xs-6 h2", text: product.name
+    end
+  
+    scenario "Show media-body for product page" do
+      expect(page).to have_selector ".media-body h2", text: product.name
+      expect(page).to have_selector ".media-body h3", text: product.display_price
+      expect(page).to have_selector ".media-body p", text: product.description
+    end
+  
+    scenario "The link to '一覧ページに戻る' is correct" do
+      expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
+    end
   end
 end
