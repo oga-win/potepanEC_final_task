5d103cdc1f9a468606b0f6d8cd72c036ba1fa8c1
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-14 17:14:07 +0900

    不要スペース削除

--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -10,24 +10,24 @@ RSpec.feature "ProductsFeatures", type: :feature do
   describe "About taxonomy" do
     before { visit potepan_product_path(product.id) }
 
-      scenario "Show page title for product page" do
+    scenario "Show page title for product page" do
       expect(page).to have_title "#{product.name} - BIGBAG Store"
     end
-  
+
     scenario "The page title for product page" do
       expect(page).to have_selector ".page-title h2", text: product.name
     end
-  
+
     scenario "Show right section product name for product page" do
       expect(page).to have_selector ".col-xs-6 h2", text: product.name
     end
-  
+
     scenario "Show media-body for product page" do
       expect(page).to have_selector ".media-body h2", text: product.name
       expect(page).to have_selector ".media-body h3", text: product.display_price
       expect(page).to have_selector ".media-body p", text: product.description
     end
-  
+
     scenario "The link to '一覧ページに戻る' is correct" do
       expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
     end
