cfd596bc73babc52cef9e0ca7f9106c4f143eb25
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-16 22:19:41 +0900

    resources showのみのため[]削除

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     get :tokushoho,                 to: 'sample#tokushoho'
     get :privacy_policy,            to: 'sample#privacy_policy'
 
-    resources :products,            only: [:show]
-    resources :categories,          only: [:show]
+    resources :products,            only: :show
+    resources :categories,          only: :show
   end
 end
