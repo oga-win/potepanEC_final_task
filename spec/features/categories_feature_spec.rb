ff8a7d040dacd012be958d57f0da06a5bb4c35be
oga <ti.pit.fighter.zed.12309.739@gmail.com>
2021-05-16 14:21:10 +0900

    類似テスト統一,テスト説明文変更

--- spec/features/categories_feature_spec.rb
+++ spec/features/categories_feature_spec.rb
@@ -12,33 +12,14 @@ RSpec.feature "CategoriesFeatures", type: :feature do
   describe "About taxonomy" do
     before { visit potepan_category_path(taxonomy.root.id) }
 
-    scenario "Show page-title on the category page" do
+    scenario "Confirm that the display of the category page is correct" do
       expect(page).to have_title "#{taxonomy.root.name} - BIGBAG Store"
-    end
-
-    scenario "Show sidebar-title on the category page" do
       expect(page).to have_selector ".page-title h2", text: "Category"
-    end
-
-    scenario "Show breadcrumbs on the category page" do
       expect(page).to have_selector ".col-xs-6 .breadcrumb", text: "Home Shop Category"
-    end
-
-    scenario "List products that belong to the same taxon" do
       expect(page).to have_selector ".panel-body", text: bag.name
       expect(page).to have_selector ".panel-body", text: shirts.name
-    end
-
-    scenario "Show product-name on the category page" do
       expect(page).to have_selector ".productCaption h5", text: product.name
-    end
-
-    scenario "Show product-price on the category page" do
       expect(page).to have_selector ".productCaption h3", text: product.display_price
-    end
-
-    scenario "The link to the top page is correct" do
-      expect(page).to have_link "Home", href: potepan_path
     end
 
     scenario "It is possible to page transition when clicked product detail page" do
@@ -54,11 +35,9 @@ RSpec.feature "CategoriesFeatures", type: :feature do
     end
 
     scenario "The link to list products that belong to the same taxon is correct" do
+      expect(page).to have_link "Home", href: potepan_path
       expect(page).to have_link bag.name, href: potepan_category_path(bag.id)
       expect(page).to have_link shirts.name, href: potepan_category_path(shirts.id)
-    end
-
-    scenario "The link to the product page is correct" do
       expect(page).to have_link product.name, href: potepan_product_path(product.id)
     end
   end
@@ -66,16 +45,10 @@ RSpec.feature "CategoriesFeatures", type: :feature do
   describe "About bag" do
     before { visit potepan_category_path(bag.id) }
 
-    scenario "Show page-title on the bag page" do
+    scenario "Confirm that the display of the bag page is correct" do
       expect(page).to have_title "#{bag.name} - BIGBAG Store"
-    end
-
-    scenario "Show right section text on the bag page" do
       expect(page).not_to have_selector ".page-title h2", text: "clothing"
       expect(page).to have_selector ".page-title h2", text: "bag"
-    end
-
-    scenario "Show production caption on the bag page" do
       expect(page).not_to have_selector ".productCaption h5", text: product.name
       expect(page).not_to have_selector ".productCaption h3", text: product.display_price
     end
--- spec/features/products_feature_spec.rb
+++ spec/features/products_feature_spec.rb
@@ -10,19 +10,10 @@ RSpec.feature "ProductsFeatures", type: :feature do
   describe "About taxonomy" do
     before { visit potepan_product_path(product.id) }
 
-    scenario "Show page title for product page" do
+    scenario "The display on the product page is correct" do
       expect(page).to have_title "#{product.name} - BIGBAG Store"
-    end
-
-    scenario "The page title for product page" do
       expect(page).to have_selector ".page-title h2", text: product.name
-    end
-
-    scenario "Show right section product name for product page" do
       expect(page).to have_selector ".col-xs-6 h2", text: product.name
-    end
-
-    scenario "Show media-body for product page" do
       expect(page).to have_selector ".media-body h2", text: product.name
       expect(page).to have_selector ".media-body h3", text: product.display_price
       expect(page).to have_selector ".media-body p", text: product.description
@@ -30,6 +21,11 @@ RSpec.feature "ProductsFeatures", type: :feature do
 
     scenario "The link to '一覧ページに戻る' is correct" do
       expect(page).to have_link '一覧ページへ戻る', href: potepan_category_path(product.taxons.first.id)
+    end
+
+    scenario "It is possible to page transition when clicked product detail page" do
+      click_on '一覧ページへ戻る'
+      expect(current_path).to eq potepan_category_path(product.taxons.first.id)
     end
   end
 end
